a235a8126b0414af592f19d42d3ead5e870f7b95
Anh Hoang <anh.comdr@gmail.com>
2026-08-09 23:04:12 +0300

    Update code

--- Labfiles/text-agent/Python/agents.py
+++ Labfiles/text-agent/Python/agents.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 # Add references
 from azure.identity import DefaultAzureCredential
-from azure.ai.projects import AIProjectsClient
+from azure.ai.projects import AIProjectClient
 
 
 load_dotenv()
@@ -15,24 +15,29 @@ def main():
         os.system('cls' if os.name == 'nt' else 'clear')
         # Load Configuration from environment variables
         load_dotenv()
-        foundry_endpoint = os.getenv("AZURE_FOUNDRY_ENDPOINT")
+        foundry_endpoint = os.getenv("PROJECT_ENDPOINT")
         agent_name = os.getenv("AZURE_AGENT_NAME")
-
+        # model_deployment = os.getenv("MODEL_DEPLOYMENT_NAME")
         # Get project client
-        project_client = AIProjectsClient(credential=DefaultAzureCredential(), 
+        project_client = AIProjectClient(credential=DefaultAzureCredential(), 
                                           endpoint=foundry_endpoint)
         # Get an OpenAI client
         openai_client = project_client.get_openai_client()
 
         # Use the agent to get a response
         prompt = input("Enter your prompt: ")
-        response = openai_client.get_response(
+        response = openai_client.responses.create(
             input=[{"role": "user", "content": prompt}],
-            extra_body={"agent_reference": {"name": agent_name, "type": "agent_reference"}}
+            extra_body={"agent_reference": {
+                "name": agent_name, 
+                "type": "agent_reference", 
+                # "deployment": model_deployment
+                }
+            }
         )
         print(f"{agent_name} Response: {response.output_text}")
 
     except Exception as e:
         print(f"Error: {e}")
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
